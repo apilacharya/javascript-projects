09e7b0b4083cddb11485f702e92d526c06e90ea2
Apil Acharya <connectwithapil@gmail.com>
2020-10-21 08:50:12 +0545

    final commit

---
+++ music player/script.js
@@ -0,0 +1,120 @@
+import songs from './songs.js';
+
+const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
+
+//check if playing
+let isPlaying = false;
+//play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
+  music.play();
+}
+
+//pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+
+  music.pause();
+}
+
+//play or pause event listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+//update dom
+
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+//current song
+let songIndex = 0;
+
+//next song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+//previous song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// on load - select
+loadSong(songs[songIndex]);
+
+//update progress bar & time
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+    //update progress bar width
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    //calculate display for duration
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+
+    //delay switching duration Element to avoid NaN
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+
+    //calculate display for  current display
+    const currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+  }
+}
+
+//set progress bar
+
+function setProgressBar(e) {
+  const width = this.clientWidth
+  const clickX = e.offsetX
+  const {duration} = music
+  music.currentTime = (clickX / width) * duration
+
+}
+
+//event listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar)
+music.addEventListener('ended', nextSong)
+
+//diff between innertext and textcontent
+//they are easily confused  innertext reflows the dom but text content
---
+++ music player/songs.js
@@ -0,0 +1,52 @@
+//music
+const songs = [
+  {
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
+  },
+
+  {
+    name: 'yemale',
+    displayName: 'Mai Pani Yemale',
+    artist: 'Badri Pangeni',
+  },
+
+  {
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
+  },
+
+  {
+    name: 'same_beef',
+    displayName: 'Bohemia Same Beef',
+    artist: 'Bohemia, Dillijit',
+  },
+
+  {
+    name: 'jacinto-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Jacinto Design',
+  },
+
+  {
+    name: 'aag',
+    displayName: 'Jay Shiva Shankhar',
+    artist: 'Samuel Shankhar',
+  },
+
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
+
+  {
+    name: 'bewafa',
+    displayName: 'Bewafa_Imran Khan',
+    artist: 'Imran Khan',
+  },
+];
+
+export default songs;
